3f5721a0813cf593181b49699f8371031d5b8398
Adrian Ivanescu <ivanescuadrian2012@gmail.com>
2021-11-29 21:30:25 +0200

    add_alt_image

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -4,10 +4,17 @@ import "./RecipeTile.css";
 export default function RecipeTile({ recipe }) {
   return (
     recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
-      <div className="recipeTile" onClick={()=>{
-        window.open(recipe["recipe"]["url"])
-      }}>
-        <img calssName="recipeTile__img" src={recipe["recipe"]["image"]} />
+      <div
+        className="recipeTile"
+        onClick={() => {
+          window.open(recipe["recipe"]["url"]);
+        }}
+      >
+        <img
+          calssName="recipeTile__img"
+          src={recipe["recipe"]["image"]}
+          alt="recipe"
+        />
         <p className="recipeTile__name"> {recipe["recipe"]["label"]}</p>
       </div>
     )
